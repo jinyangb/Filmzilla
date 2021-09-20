b6b748f6fad7eeb25fd1e05d9f7c05e50e4b600e
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-09-20 11:02:11 -0600

    added userId to reviews model '

---
+++ models/review.js
@@ -0,0 +1,33 @@
+'use strict'
+const { Model } = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Review.init(
+    {
+      description: DataTypes.STRING,
+      rating: DataTypes.INTEGER,
+      userId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'users',
+          id: 'id'
+        }
+      }
+    },
+    {
+      sequelize,
+      modelName: 'Review',
+      tableName: 'reviews'
+    }
+  )
+  return Review
+}
