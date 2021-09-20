385b63cd894248144937e72363f9cfb9c5462918
rcraig <ryanpcraig1@gmail.com>
2021-09-20 12:30:39 -0500

    started putting together the pages and nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
-
+import { Route, Switch, withRouter} from 'react-router-dom';
+import Home from './components/pages/Home';
+import Movies from './components/pages/Movies';
+import ProfilePage from './components/pages/ProfilePage';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
     <div>
+      <MainNavigation />
+      <Switch>
+        <Route path='/' exact>
+          <Home />
+        </Route>
+        <Route path='/movies' exact>
+          <Movies />
+        </Route>
+        <Route path='/profile-page' exact>
+          <ProfilePage />
+        </Route>
+      </Switch>
     </div>
   );
 }
