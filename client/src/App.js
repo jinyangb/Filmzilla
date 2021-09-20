827dcf0942f04df318c4285dfc8e9c60767efdaf
rcraig <ryanpcraig1@gmail.com>
2021-09-20 15:13:12 -0500

    recent changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Route, Switch, withRouter} from 'react-router-dom';
+import { Route, Switch, withRouter } from 'react-router-dom';
 import Home from './components/pages/Home';
 import Movies from './components/pages/Movies';
 import ProfilePage from './components/pages/ProfilePage';
--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import ReviewList from '../ReviewList';
 
 function HomePage() {
 
   return (
     <div>
       Home Page
-      
+      <ReviewList />
     </div>
   )
 }
