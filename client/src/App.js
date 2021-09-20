4e1362a82707beab5fe5d0586672b47c4f65a67e
rcraig <ryanpcraig1@gmail.com>
2021-09-20 12:47:36 -0500

    updates to the react pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import { Route, Switch, withRouter} from 'react-router-dom';
 import Home from './components/pages/Home';
 import Movies from './components/pages/Movies';
 import ProfilePage from './components/pages/ProfilePage';
+import Register from './components/pages/Register';
+import SignIn from './components/pages/SignIn';
 import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
@@ -18,9 +20,15 @@ function App() {
         <Route path='/profile-page' exact>
           <ProfilePage />
         </Route>
+        <Route path='/register' exact>
+          <Register />
+        </Route>
+        <Route path='/sign-in' exact>
+          <SignIn />
+        </Route>
       </Switch>
     </div>
   );
 }
 
-export default App;
+export default withRouter(App);
