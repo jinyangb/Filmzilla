28533ae54e7a788b916f4565260468dc98f25647
jinyangb <your_email@example.com>
2021-09-21 11:38:09 -0400

    crud working on movie controller

--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -2,11 +2,7 @@ const Router = require('express').Router()
 const controller = require('../controllers/UserController')
 
 // Router.get('/', controller.GetProfiles)
-<<<<<<< HEAD
-Router.get('/:user_id', controller.GetUserProfile)
-=======
 Router.get('/myprofile/:user_id', controller.GetUserProfile)
->>>>>>> 9e44ccad165b0482007c7dd7083c0e5ae7527017
 // Router.post('/', controller.CreateUser)
 
 module.exports = Router
