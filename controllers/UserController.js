68cf7bfb4719217e68ace09c5b6f5e678f5a311f
Brandon Hernandez <brandoncinthia0621@gmail.com>
2021-09-21 08:40:27 -0600

    no changes

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,23 +1,37 @@
 const Users = require('../models')
 // const Review = require('../models/review')
 
-// const GetProfiles = async (req, res) => {
+const GetProfiles = async (req, res) => {
+  try {
+    const users = await Users.findAll()
+    res.send(users)
+  } catch (error) {
+    throw error
+  }
+}
+
+// const GetUserProfile = async (req, res) => {
 //   try {
-//     const users = await User.findAll()
-//     res.send(users)
+//     const userAndReviews = await Users.findByPk(req.params.user_id)
+//     res.send(userAndReviews)
 //   } catch (error) {
 //     throw error
 //   }
 // }
 
-const GetUserProfile = async (req, res) => {
-  try {
-    const userAndReviews = await Users.findByPk(req.params.user_id)
-    res.send(userAndReviews)
-  } catch (error) {
-    throw error
-  }
-}
+// const GetUserProfile = async (req, res) => {
+//   try {
+//     let profile = req.params.id
+//     const result = await Users.findOne({
+//       where: {
+//         id: profile
+//       }
+//     })
+//     res.send(result)
+//   } catch (error) {
+//     throw error
+//   }
+// }
 // const CreateUser = async (req, res) => {
 //   try {
 //     const newUser = await User.create(req.body)
@@ -28,7 +42,7 @@ const GetUserProfile = async (req, res) => {
 // }
 
 module.exports = {
-  //   GetProfiles,
-  GetUserProfile
+  GetProfiles
+  // GetUserProfile
   //   CreateUser
 }
--- routes/UserRouter.js
+++ routes/UserRouter.js
@@ -1,8 +1,8 @@
 const Router = require('express').Router()
 const controller = require('../controllers/UserController')
 
-// Router.get('/', controller.GetProfiles)
-Router.get('/myprofile/:user_id', controller.GetUserProfile)
+Router.get('/', controller.GetProfiles)
+// Router.get('/myprofile/:user_id', controller.GetUserProfile)
 // Router.post('/', controller.CreateUser)
 
 module.exports = Router
