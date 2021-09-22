55747d6257618f5fe16c26d3ffc6b24a5031d746
rcraig <ryanpcraig1@gmail.com>
2021-09-22 14:45:01 -0500

    styled the sign in form

--- client/src/components/pages/Register.js
+++ client/src/components/pages/Register.js
@@ -20,9 +20,7 @@ export default function Register(props) {
   })
 
   const handleChange = (e) => {
-    // console.log(e.target.name)
     setFormValues({ ...formValues, [e.target.name]: e.target.value })
-    console.log(formValues)
   }
 
   const handleSubmit = async (e) => {
@@ -46,58 +44,24 @@ export default function Register(props) {
                 <Form.Label>Username</Form.Label>
                 <Form.Control onChange={handleChange} name="username" type="text" placeholder="John Smith" value={formValues.username} required/>
               </Form.Group>
-              {/* <label htmlFor="name">Username</label> */}
-              {/* <input
-                onChange={handleChange}
-                name="username"
-                type="text"
-                placeholder="John Smith"
-                value={formValues.username}
-                required
-              /> */}
             </div>
             <div className="input-wrapper">
               <Form.Group className="mb-3">
                 <Form.Label>Email</Form.Label>
                 <Form.Control onChange={handleChange} name="email" type="email" placeholder="example@example.com" value={formValues.email} required/>
               </Form.Group>
-              {/* <label htmlFor="email">Email</label>
-              <input
-                onChange={handleChange}
-                name="email"
-                type="email"
-                placeholder="example@example.com"
-                value={formValues.email}
-                required
-              /> */}
             </div>
             <div className="input-wrapper">
               <Form.Group className="mb-3">
                 <Form.Label>Password</Form.Label>
                 <Form.Control onChange={handleChange} type="password" name="password" value={formValues.password} required/>
               </Form.Group>
-              {/* <label htmlFor="password">Password</label>
-              <input
-                onChange={handleChange}
-                type="password"
-                name="password"
-                value={formValues.password}
-                required
-              /> */}
             </div>
             <div className="input-wrapper">
               <Form.Group className="mb-3">
                 <Form.Label>Confirm Password</Form.Label>
                 <Form.Control onChange={handleChange} type="password" name="confirmPassword" value={formValues.confirmPassword} required/>
               </Form.Group>
-              {/* <label htmlFor="confirmPassword">Confirm Password</label>
-              <input
-                onChange={handleChange}
-                type="password"
-                name="confirmPassword"
-                value={formValues.confirmPassword}
-                required
-              /> */}
             </div>
             <Button type="submit" disabled={!formValues.email || (!formValues.password && formValues.confirmPassword === formValues.password)}>Sign In</Button>
           </Form>
