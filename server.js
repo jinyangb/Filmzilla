2bc284a8b12f201996de39f90d01324ddbb0991f
jinyangb <your_email@example.com>
2021-09-24 12:30:44 -0400

    deploy heroku

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
-const app = require("express")();
-const bodyParser = require("body-parser");
-const cors = require("cors");
-const logger = require("morgan");
+const app = require('express')()
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const logger = require('morgan')
+const path = require('path')
 
-const AppRouter = require("./routes/AppRouter");
-const MovieRouter = require("./routes/MovieRouter");
-const ReviewRouter = require("./routes/ReviewRouter");
-const UserRouter = require("./routes/UserRouter");
+const AppRouter = require('./routes/AppRouter')
+const MovieRouter = require('./routes/MovieRouter')
+const ReviewRouter = require('./routes/ReviewRouter')
+const UserRouter = require('./routes/UserRouter')
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3001
 
-app.use(cors());
-app.use(logger("dev"));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors())
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => res.json({ message: "Server Works" }));
-app.use("/api", ReviewRouter, MovieRouter, UserRouter);
-app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`));
+app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+app.use('/api', ReviewRouter, MovieRouter, UserRouter)
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(`${__dirname}/client/build/index.html`))
+  })
+}
+
+app.listen(PORT, () => console.log(`Server Started On Port: ${PORT}`))
