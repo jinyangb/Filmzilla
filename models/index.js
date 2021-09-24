c92b93ec1b8f30b9ef7613fa5682163848e502d4
jinyangb <your_email@example.com>
2021-09-24 13:00:16 -0400

    fixed config.js

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 'use strict'
-const config = require(__dirname + '/../config/config.js')[env]
+
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const app = require('express')()
+const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const logger = require('morgan')
@@ -10,7 +10,7 @@ const ReviewRouter = require('./routes/ReviewRouter')
 const UserRouter = require('./routes/UserRouter')
 
 const PORT = process.env.PORT || 3001
-
+const app = express()
 app.use(cors())
 app.use(logger('dev'))
 app.use(bodyParser.json())
